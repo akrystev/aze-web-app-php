444a14c849e08a442bea4276e654860127260d9f
akrystev <72188564+akrystev@users.noreply.github.com>
2020-10-01 10:27:47 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -21,7 +21,7 @@
         <title>Top 10 cities in Bulgaria</title>
     </head>
     <body>
-        <h1>Top 10 cities in Bulgaria</h1>
+        <h3>Top 10 cities in Bulgaria</h3>
         <br />
         <table border="1">
 <?php
